005327f4821ce23182e8e0a80e399e2fe675f13b
Angel Del Castillo <jubu201504@gmail.com>
2016-12-23 03:34:10 -0600

    bench template vs raw

--- srest_bench_test.go
+++ srest_bench_test.go
@@ -1,24 +1,12 @@
 package srest
 
 import (
+	"html/template"
 	"net/http/httptest"
 	"testing"
 )
 
 func BenchmarkRender(b *testing.B) {
-	//	dir, err := os.Getwd()
-	//	if err != nil {
-	//		b.Errorf("get pwd : err [%s]", err)
-	//	}
-	//
-	//	tmplInited = false
-	//	funcm := deffuncmap()
-	//	err = LoadViews(dir+"/mock", funcm)
-	//	if err != nil {
-	//		b.Errorf("LoadViews : err [%s]", err)
-	//		return
-	//	}
-
 	w := httptest.NewRecorder()
 	v := struct{}{}
 	b.ReportAllocs()
@@ -43,3 +31,61 @@ func BenchmarkJSON(b *testing.B) {
 		}
 	}
 }
+
+func init() {
+	t := template.Must(template.New("hello").Parse(aCase))
+	var err error
+	tA, err = t.Parse(header)
+	if err != nil {
+		panic(err)
+	}
+
+	tB = template.Must(template.New("hello").Parse(bCase))
+}
+
+var (
+	tA *template.Template
+	tB *template.Template
+)
+
+func BenchmarkCaseA(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		w := httptest.NewRecorder()
+		err := tA.Execute(w, "")
+		if err != nil {
+			b.Fatal(err)
+		}
+		// log.Printf("body [%s]", w.Body.String())
+	}
+}
+
+func BenchmarkCaseB(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		w := httptest.NewRecorder()
+		err := tB.Execute(w, "")
+		if err != nil {
+			b.Fatal(err)
+		}
+		// log.Printf("body [%s]", w.Body.String())
+	}
+}
+
+const (
+	header = `{{define "header"}}hello im header.{{end}}`
+
+	// aCase template execution.
+	aCase = `
+		hello world A
+		{{template "header" . }}
+	`
+
+	// bCase html inside.
+	bCase = `
+		hello world B
+		hello im header
+	`
+)
